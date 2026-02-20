8cc25d1e5c2cda3c181eb0d54e2bbc84ec2834c6
nasru <nasrudheennasru15@gmail.com>
2026-02-20 20:53:45 +0000

    File changes

--- src/pages/Personnel.jsx
+++ src/pages/Personnel.jsx
@@ -49,17 +49,7 @@ export default function Personnel() {
   const { data: allUsers = [], isLoading } = useQuery({
     queryKey: ['all-users'],
     queryFn: async () => {
-      // Fetch users based on current user's role
-      if (['super_admin', 'admin', 'academic_head'].includes(currentUser?.app_role)) {
-        return base44.entities.User.list('-created_date', 1000);
-      } else {
-        // For other roles, fetch specific role types they can see
-        const roles = ['junior_mentor', 'senior_mentor', 'academic_head', 'academic_admin', 'broker_admin', 'finance_admin', 'assistance'];
-        const usersByRole = await Promise.all(
-          roles.map(role => base44.entities.User.filter({ app_role: role }, '-created_date', 1000))
-        );
-        return usersByRole.flat();
-      }
+      return base44.entities.User.list('-created_date', 1000);
     },
     enabled: !!currentUser
   });

b44e78930a9aa9200f8214540c40eadbaa785e22
nasru <nasrudheennasru15@gmail.com>
2026-02-20 22:47:08 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -67,7 +67,6 @@ import MyStudentRequests from './pages/MyStudentRequests';
 import MyTargets from './pages/MyTargets';
 import Personnel from './pages/Personnel';
 import QuarterClosing from './pages/QuarterClosing';
-import RetentionManagement from './pages/RetentionManagement';
 import StudentDetail from './pages/StudentDetail';
 import StudentLogs from './pages/StudentLogs';
 import StudentRequestApprovals from './pages/StudentRequestApprovals';
@@ -75,6 +74,7 @@ import Students from './pages/Students';
 import TargetsManagement from './pages/TargetsManagement';
 import Tickets from './pages/Tickets';
 import Transactions from './pages/Transactions';
+import RetentionManagement from './pages/RetentionManagement';
 import __Layout from './Layout.jsx';
 
 
@@ -99,7 +99,6 @@ export const PAGES = {
     "MyTargets": MyTargets,
     "Personnel": Personnel,
     "QuarterClosing": QuarterClosing,
-    "RetentionManagement": RetentionManagement,
     "StudentDetail": StudentDetail,
     "StudentLogs": StudentLogs,
     "StudentRequestApprovals": StudentRequestApprovals,
@@ -107,6 +106,7 @@ export const PAGES = {
     "TargetsManagement": TargetsManagement,
     "Tickets": Tickets,
     "Transactions": Transactions,
+    "RetentionManagement": RetentionManagement,
 }
 
 export const pagesConfig = {

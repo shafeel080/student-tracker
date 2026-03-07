2290d3f5bad2cc9828f961c4d025bba552ccd8d7
nasru <nasrudheennasru15@gmail.com>
2026-03-07 23:48:17 +0000

    Manual code change

--- src/pages/QuarterClosing.jsx
+++ src/pages/QuarterClosing.jsx
@@ -34,7 +34,7 @@ export default function QuarterClosing() {
 
   const { data: users = [] } = useQuery({
     queryKey: ['users'],
-    queryFn: () => base44.entities.User.list(),
+    queryFn: async () => { const r = await base44.functions.invoke('getAllUsers', {}); return r.data?.users || []; },
     enabled: !!currentUser
   });
 

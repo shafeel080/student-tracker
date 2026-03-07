08064db0a9e33f6427d720be803467420c4e5ca6
nasru <nasrudheennasru15@gmail.com>
2026-03-07 21:54:00 +0000

    Manual code change

--- src/pages/StudentDetail.jsx
+++ src/pages/StudentDetail.jsx
@@ -45,7 +45,7 @@ export default function StudentDetail() {
 
   const { data: users = [] } = useQuery({
     queryKey: ['users'],
-    queryFn: () => base44.entities.User.list(),
+    queryFn: async () => { const r = await base44.functions.getAllUsers(); return r.users || []; },
     enabled: !!currentUser
   });
 

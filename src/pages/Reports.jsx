53288b4d1f263fda4ec8b4e0477e46a907a04191
nasru <nasrudheennasru15@gmail.com>
2026-04-02 01:03:40 +0000

    File changes

--- src/pages/Reports.jsx
+++ src/pages/Reports.jsx
@@ -107,6 +107,8 @@ export default function Reports() {
             );
         } else if (mentorFilter) {
             filtered = filtered.filter(r =>
+                r.primary_mentor_name === mentorFilter ||
+                r.senior_mentor_name === mentorFilter ||
                 r.initiating_mentor_name === mentorFilter
             );
         }
